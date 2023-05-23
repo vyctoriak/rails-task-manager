c9b8a0724412038a8a0d9cc68947a97a08f8660d
vyctoria karina <vyctoriak@gmail.com>
2023-05-23 14:17:10 +0200

    route, action and view to display a specific task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
