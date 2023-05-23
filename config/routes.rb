e87c558ee55853fa07d3e9ad88350b9890ee117f
vyctoria karina <vyctoriak@gmail.com>
2023-05-23 16:05:23 +0200

    CRUD completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/new', to: 'tasks#new'
-  post '/tasks', to: 'tasks#create'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # Read all
+  get "tasks", to: "tasks#index"
+  # Create
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # Read one
+  get "tasks/:id", to: "tasks#show", as: :task
+  # Update
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
