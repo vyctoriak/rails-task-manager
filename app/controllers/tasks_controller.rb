1d63d74e7cfec5e5df78de7e74027df3c0d7400c
vyctoria karina <vyctoriak@gmail.com>
2023-05-23 15:18:51 +0200

    route, action and view to create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,15 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:task][:title], details: params[:task][:details],
+                     completed: params[:task][:completed])
+    @task.save
+    redirect_to task_path(@task)
+  end
 end
